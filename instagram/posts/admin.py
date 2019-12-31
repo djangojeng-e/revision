eb774d921cf9427778ce18819f5a6b1652e377b6
djangojeng-e <headfat1218@gmail.com>
2019-12-31 14:52:41 +0900

    Added models to admin.

--- instagram/posts/admin.py
+++ instagram/posts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import PostComment, PostImage, Post, PostLike
+
+
+@admin.register(Post)
+class PostAdmin(admin.decorators):
+    pass
+
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PostComment)
+class PostCommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PostLike)
+class PostLikeAdmin(admin.ModelAdmin):
+    pass
+
