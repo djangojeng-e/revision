7497fa64cc16a2ac4049db60b038a44c165c6960
djangojeng-e <headfat1218@gmail.com>
2019-12-31 15:28:22 +0900

    Uploaded a photo for testing.

--- instagram/posts/admin.py
+++ instagram/posts/admin.py
@@ -7,6 +7,21 @@ from .models import PostComment, PostImage, Post, PostLike
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    """
+    Model
+    - Posts 의 __str__을 적절히 작성한다.
+
+    Admin
+    - 작성자, 글, 작성시간이 보여지게 한다
+        list_display
+
+    - 상세화면에서 PostImage를 곧바로 추가할수 있도록 한다.
+        inlines
+        TabularInline(의의 PostImageInline을 적절히 채운후 사용)
+
+    - 마찬가지로 PostComment 도 곧바로 추가할수 있도록 한다.
+    """
+    list_display = ('author', 'content', 'created')
     pass
 
 
