19732524781428567c33b789d9f4a6ebcf3947f9
djangojeng-e <headfat1218@gmail.com>
2020-01-02 14:27:29 +0900

    edited views.py in config, members and posts to allow listing out the posts for logged in users.

--- instagram/config/views.py
+++ instagram/config/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
+
+    if request.user.is_authenticated:
+        return redirect('posts:post-list')
     return render(request, 'index.html')
--- instagram/posts/views.py
+++ instagram/posts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post
 
 
 def post_list(request):
-
-    return render(request, 'posts/post-list.html')
+    posts = Post.objects.order_by('-pk')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/post-list.html', context)
