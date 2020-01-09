e4b93212c15dbaee9e0305e811ba9f30eb468928
djangojeng-e <headfat1218@gmail.com>
2020-01-09 23:05:33 +0900

    SingupForm 과제 추가.

--- instagram/members/forms.py
+++ instagram/members/forms.py
@@ -14,17 +14,17 @@ class SignupForm(forms.Form):
     email = forms.EmailField()
     name = forms.CharField()
     username = forms.CharField()
-    password = forms.CharField(max_length=12, widget=PasswordInput,required=True)
+    password = forms.CharField(max_length=12, widget=PasswordInput, required=True)
 
     def save(self, email, name, username, password):
-        if User.objects.filter(email=email):
-            return HttpResponse('이미 존재하는 email입니다!')
-        if User.objects.filter(username=username):
-            return HttpResponse('이미 존재하는 username입니다.')
-        else:
-            return User.objects.create_user(
-                email=self.cleaned_data['email'],
-                name=self.cleaned_data['name'],
-                username=self.cleaned_data['username'],
-                password=self.cleaned_data['password'],
-            )
+        """
+        Form 으로 전달받은 데이터를 사용 해서
+        새로운 User를 생성하고 리턴
+
+        Username과 email 검증로직도 이 안에 넣기
+        :param email:
+        :param name:
+        :param username:
+        :param password:
+        :return:
+        """
--- instagram/members/views.py
+++ instagram/members/views.py
@@ -21,23 +21,29 @@ def login_view(request):
 
 
 def signup_view(request):
+    '''
+    Template : index.html 을 그대로 사용
+    action만 이쪽으로
+    URL : /members/signup/
+    FORM : members.forms.SignupForm
+
+    User에 name필드를 추가
+    email
+    username
+    name
+    password
+    를 전달받아, 새로운 User를 생성한다.
+    생성시 User.objects.create.user() 메서드를 사용한다.
+
+    이미 존재하는 username 또는 email을 입력한 경우,
+    "이미 사용중인 username/email입니다" 라는 메시지를 HttpResponse로 돌려준다
+
+    생성에 성공하면 로그인 처리 후 위의 (login_view를 참조) posts:post-list로 redirect 처리
+    :param request:
+    :return:
+    '''
     if request.method == "POST":
-        form = SignupForm(data=request.POST)
 
-        if form.is_valid():
-            email = request.POST['email']
-            name = request.POST['name']
-            username = request.POST['username']
-            password = request.POST['password']
-            
-            form.save(email=email, name=name, username=username, password=password)
-            user = User.objects.get(email=email)
-            login(request, user)
-            return redirect('posts:post-list')
-        else:
-            form = SignupForm()
-
-        return render(request, 'members:login', {'form': form})
     # email = request.POST['email']
     # username = request.POST['username']
     # name = request.POST['name']
@@ -59,5 +65,13 @@ def signup_view(request):
 
 
 def logout_view(request):
+    """
+    GET요청으로 처리함
+    요청에 있는 사용자를 logout 처리
+    django.contrib.auth.logout 함수를 사용한다.
+    :param request:
+    :return:
+    """
+
     logout(request)
     return redirect('members:log_in')
